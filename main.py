058fc49dfa378b4d6571503d9cdf2398270c6adf
ReDK121 <77736884+ReDK121@users.noreply.github.com>
2022-09-04 21:43:46 +0200

    Add files via upload
    Day 6: kivy app - more features on

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+from zoneinfo import available_timezones
 import kivy
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, ScreenManagerException, Screen
+from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+import json, glob
 import json
 from datetime import datetime
+from pathlib import Path
+import random
 
 Builder.load_file("design.kv")
 
@@ -20,18 +27,16 @@ class LoginScreen(Screen):
             self.ids.login_wrong.text= "Wrong username or password!"
 
 
-
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
         with open("users.json") as file:
             users = json.load(file)
-            
-
             users[uname] = {'username': uname, 'password': pword,
                 'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
-            with open("users.json", 'w') as file:
-                json.dump(users, file)
-            self.manager.current = "sign_up_screen_success"
+        with open("users.json", 'w') as file:
+            json.dump(users, file)
+        self.manager.current = "sign_up_screen_success"
+
 
 class SignUpScreenSuccess(Screen):
     def go_to_login(self):
@@ -39,17 +44,39 @@ class SignUpScreenSuccess(Screen):
         self.manager.transition.direction = 'right'
         self.manager.current = "login_screen"
 
+
 class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+        
+    def get_quote(self, feel):
+        feel = feel.lower()
+        available_feelings = glob.glob("quotes/*txt")
+        
+        available_feelings = [Path(filename).stem for filename in 
+                                available_feelings]
+        print(available_feelings)
+        if feel in available_feelings:
+            with open(f"quotes/{feel}.txt", encoding="utf8") as file:
+                #file = open(file, errors="ignore")
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another feeling."
+
+
+class ImageButton(HoverBehavior, Image, ButtonBehavior):
+    pass
 
 class RootWidget(ScreenManager):
     pass
 
+
 class MainApp(App):
     def build(self):
         return RootWidget()
+
 
 if __name__=="__main__":
     MainApp().run()
